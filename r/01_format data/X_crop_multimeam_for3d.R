f878508663439999a81c1ca6ae7f98284e5a6263
Kingsley Griffin <kingsleyg88@gmail.com>
2021-12-15 11:33:38 +0800

    trying other raster formats

--- r/01_format data/X_crop_multimeam_for3d.R
+++ r/01_format data/X_crop_multimeam_for3d.R
@@ -5,7 +5,7 @@
 # Name: Kingsley
 # Date: Dec 2021
 ##
-
+library(raster)
 test <- raster("data/spatial/rasters/SWC_Multibeam.tif")
 plot(test)
 
@@ -13,7 +13,11 @@ a_area <- extent(306000, 310000, 6220000, 6223000)
 a_rast <- crop(test, a_area)
 
 plot(a_rast)
-writeRaster(a_rast, "output/spatial/raster/area_a_multibeam_clip.tif")
+writeRaster(a_rast, format = "GTiff", overwrite = TRUE,
+            "output/spatial/raster/area_a_multibeam_clip.tif")
+writeRaster(a_rast, format = "ascii", overwrite = TRUE,
+            "output/spatial/raster/area_a_multibeam_clip.asc")
+
 
 b_area <- extent(293000, 297000, 6224000, 6227000)
 
@@ -22,4 +26,7 @@ plot(b_area, add=T)
 
 b_rast <- crop(test, b_area)
 plot(b_rast)
-writeRaster(b_rast, "output/spatial/raster/area_b_multimeam_clip.tif")
+writeRaster(b_rast, format = "GTiff", overwrite = TRUE,
+            "output/spatial/raster/area_b_multibeam_clip.tif")
+writeRaster(b_rast, format = "ascii", overwrite = TRUE,
+            "output/spatial/raster/area_b_multibeam_clip.asc")
