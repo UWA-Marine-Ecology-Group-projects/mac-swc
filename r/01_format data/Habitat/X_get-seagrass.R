37ac56c499a06c307e768c2958b3266055fd90bb
claudehspencer <claudespencer111@gmail.com>
2022-04-01 09:55:31 +0800

    small edit on seagrass locations

--- r/01_format data/Habitat/X_get-seagrass.R
+++ r/01_format data/Habitat/X_get-seagrass.R
@@ -5,7 +5,7 @@ setwd(getwd())
 
 dat <- read.csv("data/tidy/2020-2021_south-west_BOSS-BRUV.Habitat.csv")%>%
   dplyr::filter(broad.seagrasses>0,depth>30)%>%
-  dplyr::select(campaignid,sample, longitude, latitude, depth, broad.seagrasses)%>%
+  dplyr::select(campaignid,sample, longitude, latitude, depth, broad.seagrasses,date)%>%
   dplyr::mutate(id = paste(campaignid, sample, sep = "."))%>%
   dplyr::filter(!id%in%c("2021-03_West-Coast_BOSS.58","2021-03_West-Coast_BOSS.20"))%>% #remove 2 suspect samples
   glimpse()
