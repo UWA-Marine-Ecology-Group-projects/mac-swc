d198b831a6e158ed2cbac788ee8e9db0f17da4d3
claudehspencer <claudespencer111@gmail.com>
2022-07-13 09:03:08 +0800

    bathy cross section plot

--- r/02_original gams/X_bathy_cross-section.R
+++ r/02_original gams/X_bathy_cross-section.R
@@ -44,9 +44,9 @@ bath_700 <- bath_cross %>%
   dplyr::filter(Z > -700, distance.from.coast < 10) %>% # down to 800m depth and 10km from coastline
   glimpse()
 
-bath_200 <- bath_cross %>%
+bath_255 <- bath_cross %>%
   dplyr::mutate(distance.from.coast = (x - 315484)/1000) %>% # 0 sea level is coastline
-  dplyr::filter(Z > -200, distance.from.coast < 10) %>% # down to 800m depth and 10km from coastline
+  dplyr::filter(Z > -255, distance.from.coast < 10) %>% # down to 800m depth and 10km from coastline
   glimpse()
 
 multi <- raster("data/spatial/rasters/multibeam_derivatives_depth.tif")
@@ -66,41 +66,55 @@ multi_plot <- multi_cross %>%
 
 
 p1 <- ggplot() +
-  geom_rect(aes(xmin = min(bath_700$distance.from.coast), xmax = 10, ymin =-Inf, ymax = 0), fill = "#12a5db", alpha = 0.5) +
+  geom_rect(aes(xmin = min(bath_700$distance.from.coast), xmax = 5, ymin =-Inf, ymax = 0), fill = "#12a5db", alpha = 0.5) +
   geom_line(data = bath_700, aes(y = Z, x = distance.from.coast))+
   geom_ribbon(data = bath_700, aes(ymin = -Inf, ymax = Z, x = distance.from.coast), fill = "tan") +
   theme_classic() +
   scale_x_continuous(expand = c(0,0)) +
   scale_y_continuous(breaks = c(150, 0, -150, -300, -450, -600),expand = c(0,0), limits = c(-700, 240)) +
   labs(x = "Distance from coast", y = "Elevation (m)") + 
-  annotate("text", x = 3, y = 200, label = "Boranup")
+  annotate("text", x = 3, y = 200, label = "Boranup")+
+  geom_segment(aes(x = -5.556, xend = -5.556, y =-44.03002, yend = 0), color = "red")
 p1
 
 p2 <- ggplot() +
-  geom_rect(aes(xmin = min(bath_200$distance.from.coast), xmax = 10, ymin =-Inf, ymax = 0), fill = "#12a5db", alpha = 0.5) +
-  geom_line(data = bath_200, aes(y = Z, x = distance.from.coast))+
-  geom_ribbon(data = bath_200, aes(ymin = -Inf, ymax = Z, x = distance.from.coast), fill = "tan") +
+  geom_rect(aes(xmin = min(bath_255$distance.from.coast), xmax = 5, ymin =-Inf, ymax = 0), fill = "#12a5db", alpha = 0.5) + # Add ocean
+  geom_line(data = bath_255, aes(y = Z, x = distance.from.coast))+
+  geom_ribbon(data = bath_255, aes(ymin = -Inf, ymax = Z, x = distance.from.coast), fill = "tan") +
   theme_classic() +
   scale_x_continuous(expand = c(0,0)) +
-  scale_y_continuous(breaks = c(100, 0, -100 , -200),expand = c(0,0), limits = c(-200, 240)) +
+  scale_y_continuous(breaks = c(100, 0, -100 , -200),expand = c(0,0), limits = c(-255, 240)) + # Set depth/topo limits
   labs(x = "Distance from coast", y = "Elevation (m)") +
-  annotate("text", x = 3, y = 175, label = "Boranup")
+  annotate("text", x = 3, y = 175, label = "Boranup") + # Boranup label
+  geom_segment(aes(x = -23.431987, xend = -16, y = -118, yend = -118), linetype = 2, alpha = 0.5) + # 20-30 Ka BP - segment length 7.5
+  geom_segment(aes(x = -21.111987, xend = -16, y = -94, yend = -94), linetype = 2, alpha = 0.5) + # 15-17 Ka BP
+  geom_segment(aes(x = -19.255987, xend = -16, y = -63, yend = -63), linetype = 2, alpha = 0.5) + # 12-13 Ka BP
+  geom_segment(aes(x = -10, xend = 1, y = -41, yend = -41), linetype = 2, alpha = 0.5) + # 9-10 Ka BP
+  annotate("text", x = c(-13.5,-13.5, -13.5, 3.5), y = c(-118,-94,-63,-35.93282), # 3.5 from end segment to start text
+           label = c("20-30 Ka", "15-17 Ka", "12-13 Ka", "9-10 Ka"),  # Bold text
+           size = 3.5, fontface = "bold") +
+  geom_segment(aes(x = -5.556, xend = -5.556, y =-44.03002, yend = 0), color = "red") +
+  annotate("text", x = c(-33, -6.5), y = c(-170, -77), label = c("Mid-shelf sand plains", "Submerged
+  wetlands"), size = 3) + # Standard text
+  annotate("rect", xmin = -40, xmax = -25, ymin = -155, ymax = -110,  fill = "white", color = "gray33", alpha = 0.3) + # Sand plains highlight box
+  annotate("rect", xmin = -8, xmax = -5, ymin = -50, ymax = -25, fill = "white", color = "gray33", alpha = 0.3) # Wetlands highlight box
 p2
 
 p3 <- ggplot() +
-  geom_rect(aes(xmin = min(multi_plot$distance.from.coast), xmax = 10, ymin =-Inf, ymax = 0), fill = "#12a5db", alpha = 0.5) +
-  # geom_line(data = bath_200%>%dplyr::filter(distance.from.coast > -5.57), aes(y = Z, x = distance.from.coast), color = "#faeede")+
-  geom_ribbon(data = bath_200%>%dplyr::filter(distance.from.coast > -5.57), aes(ymin = -Inf, ymax = Z, x = distance.from.coast), fill = "#f0e3d3") +
+  geom_rect(aes(xmin = min(bath_255$distance.from.coast), xmax = 4, ymin =-Inf, ymax = 0), fill = "#12a5db", alpha = 0.5) +
+  geom_ribbon(data = bath_255%>%mutate(Z = ifelse((between(distance.from.coast, -60, min(multi_plot$distance.from.coast)+0.1)|between(distance.from.coast, max(multi_plot$distance.from.coast)-0.1, 10)), Z, -255)), 
+              aes(ymin = -Inf, ymax = Z, x = distance.from.coast), fill = "#f0e3d3") +
   geom_line(data = multi_plot, aes(y = multibeam_derivatives_depth, x = distance.from.coast))+
   geom_ribbon(data = multi_plot, aes(ymin = -Inf, ymax = multibeam_derivatives_depth, x = distance.from.coast), fill = "tan") +
   theme_classic() +
-  scale_x_continuous(expand = c(0,0), limits = c(min(multi_plot$distance.from.coast), 10)) +
-  scale_y_continuous(breaks = c(100, 0, -100 , -200),expand = c(0,0), limits = c(-200, 240)) +
+  scale_x_continuous(expand = c(0,0)) +
+  scale_y_continuous(breaks = c(100, 0, -100 , -200),expand = c(0,0), limits = c(-255, 240)) +
   labs(x = "Distance from coast", y = "Elevation (m)")+
-  annotate("text", x = 3, y = 175, label = "Boranup")
+  annotate("text", x = 3, y = 175, label = "Boranup")+
+  geom_segment(aes(x = -5.556, xend = -5.556, y =-44.03002, yend = 0), color = "red")
 p3
 
 plots <- p1 / p2 / p3 + plot_annotation(tag_levels = "a")
 plots
 
-ggsave("plots/original gamms/bathy-cross-section.png", plots, height = 7, width = 8)
+ggsave("plots/original gamms/bathy-cross-section.png", plots, height = 29, width = 21, units = "cm")
