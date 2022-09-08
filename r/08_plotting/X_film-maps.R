92e5d79b73f40d03fb09e875df239ed4a52bfc16
claudehspencer <claudespencer111@gmail.com>
2022-09-08 12:53:18 +0800

    fixing movie maps

--- r/08_plotting/X_film-maps.R
+++ r/08_plotting/X_film-maps.R
@@ -34,6 +34,10 @@ unique(aumpa$ZoneName)
 aumpa_c <- st_crop(aumpa, xmin = 114, xmax = 120, ymin = -40, ymax = -32)
 st_crs(aus) <- st_crs(aumpa)
 
+stateres <- st_read("data/spatial/shapefiles/All_StateReserves.shp")
+
+world <- st_read("data/spatial/shapefiles/ne_10m_admin_0_countries.shp")
+
 wampa <- st_read("data/spatial/shapefiles/WA_MPA_2020.shp")
 st_crs(wampa) <- "+proj=longlat +datum=WGS84"
 wa_sanc <- wampa %>%
@@ -51,6 +55,16 @@ bath_c <- crop(bath_r, e)
 plot(bath_c)
 bathdf <- as.data.frame(bath_c, xy = T, na.rm = T)
 
+bathl <- rast("data/spatial/rasters/tiles/bath_250_good.tif")
+aus_v <- vect(aus)
+bathl <- mask(bathl, aus_v, inverse = T)
+aumpa_v <- vect(aumpa)
+bathl_c <- crop(bathl, buffer(aumpa_v, 100000))
+bathl_c <- clamp(bathl_c, lower = -Inf, upper = 0, values = F)
+bathl_c <- terra::aggregate(bathl_c, fact = 10, fun = "mean")
+plot(bathl_c)
+bathldf <- as.data.frame(bathl_c, na.rm = T, xy = T)
+
 terrnp <- st_read(
   "data/spatial/shapefiles/Legislated_Lands_and_Waters_DBCA_011.shp")           # terrestrial reserves
 terrnp <- terrnp[terrnp$leg_catego %in% c("Nature Reserve", "National Park"), ] # exclude state forests etc
@@ -61,7 +75,7 @@ austerr <- austerr[austerr$TYPE %in% c("Nature Reserve", "National Park"), ] # e
 # Set colours
 # state terrestrial parks colours
 terr_fills <- scale_fill_manual(values = c("National Park" = "#c4cea6",
-                                            "Nature Reserve" = "#e4d0bb"), 
+                                            "Nature Reserve" = "#c4cea6"), #e4d0bb
                                 name = "Terrestrial Managed Areas")
 
 # assign commonwealth zone colours
@@ -97,56 +111,60 @@ p1 <- ggplot() +
   nmpa_fills +
   new_scale_fill() +
   geom_sf(data = wa_sanc, fill = "#bfd054", alpha = 0.4, color = NA) +
-  geom_sf(data = langspat, aes(fill = id), alpha = 0.3, colour = "gray50", 
-          size = 0.02, show.legend = F, linetype = "dotted") +
-  lang_fills +
-  new_scale_fill() +
+  # geom_sf(data = langspat, aes(fill = id), alpha = 0.3, colour = "gray50", 
+  #         size = 0.02, show.legend = F, linetype = "dotted") +
+  # lang_fills +
+  # new_scale_fill() +
   geom_sf(data = aus, fill = NA, colour = "grey80", size = 0.1) +
   geom_sf(data = terrnp, aes(fill = leg_catego), alpha = 4/5, colour = NA) +
   terr_fills +
   new_scale_fill() +
-  geom_sf(data = langspat,fill = NA, colour = "gray50", 
-          size = 0.02, show.legend = F, linetype = "dotted") +
+  # geom_sf(data = langspat,fill = NA, colour = "gray50", 
+  #         size = 0.02, show.legend = F, linetype = "dotted") +
   geom_sf(data = aus, fill = NA, colour = "grey80", size = 0.1) +
   coord_sf(xlim = c(114, 119.9), ylim = c(-35.5, -32.1)) +
   geom_text(data = lang, aes(x = x, y = y, label = language_name), fontface = "italic", size = 3) +
   labs(x = "Longitude", y = "Latitude") +
   theme_minimal() +
-  theme(axis.line=element_blank(),axis.text.x=element_blank(),
-        axis.text.y=element_blank(),axis.ticks=element_blank(),
-        axis.title.x=element_blank(),
-        axis.title.y=element_blank(),legend.position="none",
-        panel.background=element_blank(),panel.border=element_blank(),panel.grid.major=element_blank(),
-        panel.grid.minor=element_blank(),plot.background=element_blank())
+  theme(axis.line = element_blank(),axis.text.x = element_blank(),
+        axis.text.y = element_blank(),axis.ticks = element_blank(),
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),legend.position = "none",
+        panel.background = element_blank(),panel.border = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),plot.background = element_blank(),
+        plot.margin = grid::unit(c(0,0,0,0), "mm"))
 png(filename = "plots/Indigenous-language-groups.png", units = "in", res = 300,
-    width = 10, height = 6)
+    width = 10, height = 6.7)
 p1
 dev.off()
 
 p2 <- ggplot() +
-  geom_sf(data = aus, fill = "seashell2", colour = "grey80", size = 0.1) +
+  geom_raster(data = bathldf, aes(x = x, y = y, fill = bath_250_good), show.legend = F) +
+  scale_fill_gradient(low = "#062f6b", high = "#9dc9e1") +
+  new_scale_fill() +
+  geom_sf(data = world, fill = "seashell2", colour = "grey80", size = 0.1) +
   geom_sf(data = aumpa, aes(fill = ZoneName), alpha = 0.4, color = NA) +
   nmpa_fills +
   new_scale_fill() +
-  geom_sf(data = wa_sanc, fill = "#bfd054", alpha = 0.4, color = NA) +
+  geom_sf(data = stateres, fill = "#bfd054", alpha = 0.4, color = NA) +
   geom_sf(data = aus, fill = NA, colour = "grey80", size = 0.1) +
   geom_sf(data = austerr, aes(fill = TYPE), alpha = 4/5, colour = NA) +
   terr_fills +
   new_scale_fill() +
-  geom_sf(data = langspat,fill = NA, colour = "gray50", 
-          size = 0.02, show.legend = F, linetype = "dotted") +
   geom_sf(data = aus, fill = NA, colour = "grey80", size = 0.1) +
-  coord_sf() +
-  theme_minimal() + 
-  theme(axis.line=element_blank(),axis.text.x=element_blank(),
-        axis.text.y=element_blank(),axis.ticks=element_blank(),
-        axis.title.x=element_blank(),
-        axis.title.y=element_blank(),legend.position="none",
-        panel.background=element_blank(),panel.border=element_blank(),panel.grid.major=element_blank(),
-        panel.grid.minor=element_blank(),plot.background=element_blank())
-# png(filename = "plots/Indigenous-language-groups.png", units = "in", res = 300,
-#     width = 10, height = 6)
+  coord_sf(xlim = c(111, 160), ylim = c(-44, -11)) +
+  theme_minimal() +
+  theme(axis.line = element_blank(),axis.text.x = element_blank(),
+        axis.text.y = element_blank(),axis.ticks = element_blank(),
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),legend.position = "none",
+        panel.background = element_rect(fill = "#9dc9e1"),
+        panel.border = element_blank(),panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),plot.background = element_blank())
+png(filename = "plots/Aus-with-parks.png", units = "in", res = 2000,
+    width = 10, height = 7.5)
 p2
 dev.off()
 
-unique(aumpa$ZoneName)
+
