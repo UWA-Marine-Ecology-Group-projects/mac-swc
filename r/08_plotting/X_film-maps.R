46e538225e0f0bc99835e730fc603e7587b4f283
claudehspencer <claudespencer111@gmail.com>
2022-09-27 13:28:38 +0800

    updates to film maps

--- r/08_plotting/X_film-maps.R
+++ r/08_plotting/X_film-maps.R
@@ -11,12 +11,12 @@ library(tidyr)
 library(cartography)
 library(RColorBrewer)
 library(scales)
+library(colorspace)
 
 # lang <- st_read("data/spatial/shapefiles/language-groups-final.shp")
 lang <- read.csv("data/tidy/aus-language-groups.csv") %>%
   dplyr::rename(x = "approximate_longitude_of_language_variety",
                 y = "approximate_latitude_of_language_variety") %>%
-  dplyr::filter(!language_name %in% "Nyoongar") %>%
   glimpse()
 
 langspat <- st_read("data/spatial/shapefiles/language-groups-clipped.shp") %>%
@@ -68,6 +68,11 @@ bathl_c <- terra::aggregate(bathl_c, fact = 10, fun = "mean")
 plot(bathl_c)
 bathldf <- as.data.frame(bathl_c, na.rm = T, xy = T)
 
+mb <- rast("data/spatial/rasters/multibeam_derivatives_depth.tif")
+mb <- project(mb, "+proj=longlat +datum=WGS84")
+plot(mb)
+ext(mb)
+
 terrnp <- st_read(
   "data/spatial/shapefiles/Legislated_Lands_and_Waters_DBCA_011.shp")           # terrestrial reserves
 terrnp <- terrnp[terrnp$leg_catego %in% c("Nature Reserve", "National Park"), ] # exclude state forests etc
@@ -104,10 +109,11 @@ p1 <- ggplot() +
   terr_fills +
   new_scale_fill() +
   geom_sf(data = aus, fill = NA, colour = "grey80", size = 0.1) +
-  geom_sf(data = are, aes(fill = IAR_NAME21)) + 
+  # geom_sf(data = are, aes(fill = IAR_NAME21)) + 
   coord_sf(xlim = c(114, 119.9), ylim = c(-35.5, -32.1)) +
-  geom_text(data = lang, aes(x = x, y = y, label = language_name), 
-            fontface = "bold.italic", size = 5, alpha = 0.65, colour = "darkorange3") +
+  geom_text(data = lang%>%dplyr::filter(language_name %in% c("Wadandi ", "Pibelmen")), 
+            aes(x = x, y = y, label = language_name), 
+            fontface = "bold.italic", size = 7, alpha = 0.65, colour = "darkorange3") +
   labs(x = "Longitude", y = "Latitude") +
   theme_minimal() +
   theme(axis.line = element_blank(),axis.text.x = element_blank(),
@@ -123,7 +129,6 @@ png(filename = "plots/Indigenous-language-groups.png", units = "in", res = 300,
 p1
 dev.off()
 
-are <- st_read("data/spatial/shapefiles/IARE_2021_AUST_GDA2020.shp")
 
 p2 <- ggplot() +
   geom_raster(data = bathldf, aes(x = x, y = y, fill = bath_250_good), show.legend = F) +
@@ -140,9 +145,9 @@ p2 <- ggplot() +
   terr_fills +
   new_scale_fill() +
   geom_sf(data = aus, fill = NA, colour = "grey80", size = 0.1) +
-  geom_sf(data = iloc, aes(fill = ILO_NAME21)) + 
-  geom_text(data = lang, aes(x = x, y = y, label = language_name), 
-            fontface = "bold.italic", size = 1, alpha = 0.65, colour = "darkorange3") +
+  geom_point(data = lang, aes(x = x, y = y, colour = language_name),
+            size = 5, alpha = 0.15) + #, colour = "darkorange3"
+  scale_colour_discrete_sequential(palette = "YlOrBr") +
   coord_sf(xlim = c(111, 160), ylim = c(-44, -11)) +
   theme_minimal() +
   theme(axis.line = element_blank(),axis.text.x = element_blank(),
@@ -153,9 +158,46 @@ p2 <- ggplot() +
         panel.border = element_blank(),panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),plot.background = element_blank())
 
-# png(filename = "plots/Aus-with-parks.png", units = "in", res = 2000,
-#     width = 10, height = 7.5)
+png(filename = "plots/Aus-with-parks.png", units = "in", res = 200,
+    width = 10, height = 7.5)
 p2
+dev.off()
+
+p3 <- ggplot() +
+  geom_raster(data = bathdf, aes(x = x, y = y, fill = Z),show.legend = F) +
+  scale_fill_gradientn(colours = c("#062f6b", "#2b63b5","#9dc9e1"),
+                       values = rescale(c(-6221, -120, 0))) +
+  new_scale_fill() +
+  geom_sf(data = aus, fill = "seashell2", colour = "grey80", size = 0.1) +
+  geom_sf(data = aumpa_c, aes(fill = ZoneName), alpha = 0.4, color = NA) +
+  nmpa_fills +
+  new_scale_fill() +
+  geom_sf(data = wa_sanc, fill = "#bfd054", alpha = 0.4, color = NA) +
+  geom_sf(data = aus, fill = NA, colour = "grey80", size = 0.1) +
+  geom_sf(data = terrnp, aes(fill = leg_catego), alpha = 4/5, colour = NA) +
+  terr_fills +
+  new_scale_fill() +
+  geom_sf(data = aus, fill = NA, colour = "grey80", size = 0.1) +
+  # geom_sf(data = are, aes(fill = IAR_NAME21)) + 
+  coord_sf(xlim = c(114, 116.2), ylim = c(-34.6, -33.3)) +
+  geom_text(data = lang, aes(x = x, y = y, label = language_name), 
+            fontface = "bold.italic", size = 10, alpha = 0.65, colour = "darkorange3") +
+  labs(x = "Longitude", y = "Latitude") +
+  annotate("rect", xmin = 114.708118647453, xmax = 114.956063687516, 
+           ymin = -34.1400803710618, ymax = -34.0108992689951,
+           colour = "darkgoldenrod1", fill = "white", alpha = 0.2, size = 1.5) +
+  theme_minimal() +
+  theme(axis.line = element_blank(),axis.text.x = element_blank(),
+        axis.text.y = element_blank(),axis.ticks = element_blank(),
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),legend.position = "none",
+        panel.background = element_blank(),panel.border = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),plot.background = element_blank(),
+        plot.margin = grid::unit(c(0,0,0,0), "mm"))
+png(filename = "plots/Indigenous-language-groups-zoomed-multibeam.png", units = "in", res = 300,
+    width = 10, height = 6.7)
+p3
 dev.off()
 
 
